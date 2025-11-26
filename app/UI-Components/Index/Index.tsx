da7ea325ea0a68a77c63584a9c206977b9e2372d
Israel-199 <israelassefa199@gmail.com>
2025-11-26 10:03:39 -0800

    implement Faqs page

--- app/UI-Components/Index/Index.tsx
+++ app/UI-Components/Index/Index.tsx
@@ -1,9 +1,9 @@
 import About from "./About/page";
 import Expertise from "./Expertise/page";
+import Faqs from "./Faq/page";
 import Hero from "./Hero/page";
 import Paralex from "./Paralex/page";
 import PricingPlan from "./PricingPlans/page";
-
 import Services from "./Services/page";
 import Testimonial from "./Testimonials/page";
 
@@ -17,6 +17,7 @@ export default function Index() {
      <Expertise/>
      <PricingPlan/>
      <Testimonial/>
+     <Faqs/>
     </>
   )
 }
